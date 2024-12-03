49c5cc346a4748caa55237eb048fe8cec007158f
Matej Valocký <74997007+matejvalocky@users.noreply.github.com>
2024-12-03 23:13:14 +0100

    116. funkce pro výpis localStorage do stránek

--- functions.js
+++ functions.js
@@ -20,3 +20,21 @@ const getSavedNames = function (){
 const saveNames = function(oneName){
     localStorage.setItem("names", JSON.stringify(oneName))
 }
+
+
+// generovanie html štruktúry, ktorú umiestnime do stránky po kliknutí na tlačídlo vypíš + použijeme ju tiež pre vypísanie nových informácii z localStorage, keď nejaké meno vymažeme pomocou tlačídla "Vymazať meno"
+
+const generateHTMLstructure = function(oneName){
+    const newDiv = document.createElement("div")
+    const newSpan = document.createElement("span")
+    const button = document.createElement("button")
+
+    // nastavenie mazacieho tlačídla
+    button.textContent = "Vymazať meno"
+    newDiv.appendChild(button)
+
+    newSpan.textContent = oneName.firstName
+    newDiv.appendChild(newSpan)
+    return newDiv
+}
+
--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+// Načítanie dát z localStorage do premennej names, ak je localStorage prázdné, tak do names sa uloží prázdne pole
+
 const names = getSavedNames()
 
+
+
+// Odoslanie formulára a uloženie do localStorage pomocou premennej names
 let myForm = document.querySelector("#test-form")
 myForm.addEventListener("submit", function(event){
     event.preventDefault()
@@ -13,8 +18,19 @@ myForm.addEventListener("submit", function(event){
 
     saveNames(names)
 
+})
 
 
+// Vypisovanie späť do stránky
 
+let buttonToList = document.querySelector(".to-list")
 
+buttonToList.addEventListener("click", function(event){
+    let namesFromStorage = localStorage.getItem("names")
+    let namesFromStorageJSON = JSON.parse(namesFromStorage)
+
+    namesFromStorageJSON.forEach(function(myName){
+       const oneNameHTML = generateHTMLstructure(myName)
+       document.querySelector(".list-names").appendChild(oneNameHTML)
+    })
 })
