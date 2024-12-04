0900e7bc6ff81e8e4e09894b1e52d5415802267b
Matej Valocký <74997007+matejvalocky@users.noreply.github.com>
2024-12-04 20:23:31 +0100

    124. změněné jméno ukládáme do localStorage

--- script-edit.js
+++ script-edit.js
@@ -11,3 +11,17 @@ if (searchedName === undefined){
 }
 
 
+document.querySelector("#editedName").value = searchedName.firstName // zobrazí hodnotu editovaného obsahu v políčku s id editedName
+
+let changingForm = document.querySelector("#changing-form")
+
+changingForm.addEventListener("submit", function(event){
+    event.preventDefault()
+
+    
+
+    searchedName.firstName = event.target.elements.changingName.value
+
+    saveNames(names)
+
+})
