121ca24f85d508299f3739ff7df5e164327b6c64
Matej Valocký <74997007+matejvalocky@users.noreply.github.com>
2024-12-04 20:48:49 +0100

    125. data napříč záložkami v prohlížeči (event storage, window)

--- script-edit.js
+++ script-edit.js
@@ -25,3 +25,24 @@ changingForm.addEventListener("submit", function(event){
     saveNames(names)
 
 })
+
+// udalosť vznikne vtedy, keď sa čokoľvek zmení v localStorage
+window.addEventListener("storage", function(event){
+    console.log(event) // vypisuje na stránkach, kde nedochádza priamo k zmene
+
+    if (event.key === "names") {
+        names = JSON.parse(event.newValue) 
+    }
+
+    let searchedName = names.find(function(oneObject){
+        return oneObject.id === nameID
+    })
+    
+    if (searchedName === undefined){
+        location.assign("/index.html")
+    }
+    
+    
+    document.querySelector("#editedName").value = searchedName.firstName // zobrazí hodnotu editovaného obsahu v políčku s id editedName
+
+})
